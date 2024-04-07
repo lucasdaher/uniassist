101e969ffc1a83233b6ba56b41b1e3bba1b129f1
lucasdaher <contato@lucasdaher.com>
2024-04-07 14:15:11 -0300

    feat: add new images and content

---
+++ src/pages/request/request.tsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header";
+import RequestForm from "../../components/request/requestForm";
+
+const Request = () => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <RequestForm />
+      </main>
+    </div>
+  );
+};
+
+export default Request;
